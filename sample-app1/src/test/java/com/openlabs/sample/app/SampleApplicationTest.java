98095520eddb0690e2240d617102f59dff70bbda
ethan <47875390+ifubabz@users.noreply.github.com>
2020-08-19 18:23:46 +0900

    Add files via upload

---
+++ sample-app1/src/test/java/com/openlabs/sample/app/SampleApplicationTest.java
@@ -0,0 +1,14 @@
+package com.openlabs.sample.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleApplicationTest {
+
+	@Test
+	void contextLoad() {
+		
+	}
+
+}
---
+++ sample-batch1/src/test/java/com/openlabs/sample/batch/SampleBatchApplicationTest.java
@@ -0,0 +1,14 @@
+package com.openlabs.sample.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleBatchApplicationTest {
+
+	@Test
+	void contextLoad() {
+		
+	}
+
+}
